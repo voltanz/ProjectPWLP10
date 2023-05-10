6645e985b01461001c6c4e7c6a6a61cd94d47d35
voltanz <bimatris13@gmail.com>
2023-05-10 10:19:24 +0700

    Praktikum 2

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
 <div class="container">
-    <form action="/articles" method="post" enctype="multipart/formdata">
+    <form action="/articles" method="post" enctype="multipart/form-data">
         @csrf
         <div class="form-group">
             <label for="title">Title: </label>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\ArticleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::resource('articles', ArticleController::class);
+Auth::routes();
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
